b335ea3d67b94c5dd23f56569c83cbb070cabec9
Tsuyoshi Tanaka <tanakatsu1080@gmail.com>
2025-12-29 12:26:29 +0900

    Continue processing when ConnectionError occurred

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from zoneinfo import ZoneInfo
 
 import schedule
 from dotenv import load_dotenv
+from requests.exceptions import ConnectionError
 
 from amedas import AmedasDataClient
 from influxdb import InfluxDBWriter
@@ -21,20 +22,23 @@ def task_switchbot(
     """
     SwitchBotデバイスの状態を取得してInfluxDBに書き込む
     """
-    measurement = "sensor"
-    for device in device_list:
-        status = switchbot_client.get_device_status(device.device_id)
-        print(
-            f"デバイス名: {device.device_name}, デバイスID: {device.device_id}, 状態: {status}"
-        )
-        with influxdb_writer as writer:
-            writer.write(
-                measurement,
-                "sensor_id",
-                device.device_name,
-                status.temperature,
-                status.humidity,
+    try:
+        measurement = "sensor"
+        for device in device_list:
+            status = switchbot_client.get_device_status(device.device_id)
+            print(
+                f"デバイス名: {device.device_name}, デバイスID: {device.device_id}, 状態: {status}"
             )
+            with influxdb_writer as writer:
+                writer.write(
+                    measurement,
+                    "sensor_id",
+                    device.device_name,
+                    status.temperature,
+                    status.humidity,
+                )
+    except ConnectionError as e:
+        print(f"ConnectionError occurred: {e}")
 
 
 def task_amedas(
@@ -45,32 +49,35 @@ def task_amedas(
     """
     30分前のAMEDASデータを取得してInfluxDBに書き込む
     """
-    measurement = "amedas"
-    now = datetime.datetime.now(datetime.UTC).replace(second=0, microsecond=0)
-    target_time = now - datetime.timedelta(minutes=30)
-    target_time = target_time.replace(
-        minute=round(target_time.minute - 5, -1)
-    )  # 10分単位に丸める
+    try:
+        measurement = "amedas"
+        now = datetime.datetime.now(datetime.UTC).replace(second=0, microsecond=0)
+        target_time = now - datetime.timedelta(minutes=30)
+        target_time = target_time.replace(
+            minute=round(target_time.minute - 5, -1)
+        )  # 10分単位に丸める
 
-    local_tz = ZoneInfo("Asia/Tokyo")
-    local_target_time = target_time.astimezone(local_tz)
+        local_tz = ZoneInfo("Asia/Tokyo")
+        local_target_time = target_time.astimezone(local_tz)
 
-    df = amedas_client.fetch_one(local_target_time)
-    temp_c = df.iloc[0]["temp"]
-    humidity = df.iloc[0]["humidity"]
-    print(
-        f"Amedasデータ取得: {local_target_time}, temp_c: {temp_c:.2f}, humidity: {humidity:.2f}"
-    )
-
-    with influxdb_writer as writer:
-        writer.write(
-            measurement,
-            "location_id",
-            amedas_location_name,
-            temp_c,
-            humidity,
-            target_time,  # UTC時間で書き込む
+        df = amedas_client.fetch_one(local_target_time)
+        temp_c = df.iloc[0]["temp"]
+        humidity = df.iloc[0]["humidity"]
+        print(
+            f"Amedasデータ取得: {local_target_time}, temp_c: {temp_c:.2f}, humidity: {humidity:.2f}"
         )
+
+        with influxdb_writer as writer:
+            writer.write(
+                measurement,
+                "location_id",
+                amedas_location_name,
+                temp_c,
+                humidity,
+                target_time,  # UTC時間で書き込む
+            )
+    except ConnectionError as e:
+        print(f"ConnectionError occurred: {e}")
 
 
 def main():
